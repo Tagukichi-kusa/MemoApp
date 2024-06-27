56aa87259d9c30547de05920bedd8dbb86706fc3
Tagukichi-kusa <takumiyoshikin@gmail.com>
2024-06-28 00:47:16 +0900

    Markup MemoListScreen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,64 @@
-import { View,Text } from "react-native";
+import { View,Text,StyleSheet } from "react-native";
 import React from 'react';
 
 
 const Index = (): JSX.Element => {
   return(
-<View>
-    <Text>Hello INNA</Text>
+<View style={styles.container}>
+    <View>
+      <View>
+        <Text>Memo App</Text>
+        <Text>Logout</Text>
+      </View>
+    </View>
+
+    <View>
+
+      <View>
+        <View>
+          <Text>買い物リスト</Text>
+          <Text>2024/06/30 12:00</Text>
+        </View>
+        <View>
+          <Text>X</Text>
+        </View>
+      </View>
+
+      <View>
+        <View>
+          <Text>買い物リスト</Text>
+          <Text>2024/06/30 12:00</Text>
+        </View>
+        <View>
+          <Text>X</Text>
+        </View>
+      </View>
+
+      <View>
+        <View>
+          <Text>買い物リスト</Text>
+          <Text>2024/06/30 12:00</Text>
+        </View>
+        <View>
+          <Text>X</Text>
+        </View>
+      </View>
+
+    </View>
+    <View>
+      <Text>+</Text>
+    </View>
 </View>
 
   )
 }
 
+
+const styles = StyleSheet.create({
+  container:{
+    flex : 1,
+    justifyContent : 'center',
+    alignItems : 'center'
+  }
+})
 export default Index
