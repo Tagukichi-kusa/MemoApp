bb38b9188c87a335f361db044435b5671a6c9ff6
Tagukichi-kusa <takumiyoshikin@gmail.com>
2024-07-16 00:39:00 +0900

    alter header of navigation

--- src/app/memo/list.tsx
+++ src/app/memo/list.tsx
@@ -1,17 +1,29 @@
 import { View,StyleSheet } from "react-native";
 import React from 'react';
-//import { Feather } from "@expo/vector-icons";
-import Icon from "../../components/Icon"
+import { useEffect } from "react";
 
 // import Header from '../../components/Header'
 import MemoListItem from "../../components/MemoListItem";
 import CircleButton from "../../components/CircleButton";
-import { router } from "expo-router";
+import LogOutButton from "../../components/LogOutButton";
+//import { Feather } from "@expo/vector-icons";
+import Icon from "../../components/Icon"
+import { router, useNavigation } from "expo-router";
 const handlePress = ():void => {
   router.push('/memo/create')
 }
 
 const List = (): JSX.Element => {
+  //ナビゲーションのカスタマイズ（オブジェクト取得）
+  const navigation = useNavigation()
+  //初期処理（useEffect）
+  useEffect( () => {
+    navigation.setOptions({
+      // この画面だけヘッダーの右側をカスタマイズ
+      headerRight: () => {return <LogOutButton />}
+    })
+  },[])
+
   return(
 <View style={styles.container}>
 {/* Header */}
