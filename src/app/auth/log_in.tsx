fc00994d7ef82b2d2dbe3fe27283dac7da06330d
Tagukichi-kusa <takumiyoshikin@gmail.com>
2024-07-19 22:22:06 +0900

    add login blogic

--- src/app/auth/log_in.tsx
+++ src/app/auth/log_in.tsx
@@ -1,16 +1,29 @@
 import React from "react";
-import {View,Text,TextInput,
+import {View,Text,TextInput,Alert,
     TouchableOpacity,StyleSheet} from 'react-native'
 
 import { Link, router } from "expo-router";
 import { useState } from "react";
-
-// import Header from "../../components/Header";
+import { signInWithEmailAndPassword } from "firebase/auth";
 import Button from "../../components/Button";
+import { auth } from "../../config";
+// import Header from "../../components/Header";
 
-const handlePress = (): void => {
-    //ログイン処理
-    router.replace('/memo/list')
+const handlePress = (email: string,password: string): void => {
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            console.log(userCredential.user.uid)
+            //ログイン処理
+            router.replace('/memo/list')
+
+        })
+        .catch((error) => {
+            const{ code, message } = error
+            console.log(code, message)
+            Alert.alert(message)
+
+        })
+
 }
 
 const LogIn = ():JSX.Element => {
@@ -40,10 +53,10 @@ const LogIn = ():JSX.Element => {
                     placeholder='Password'          //空の時の表示名
                     textContentType='password'      //キーチェーン使用時の自動保管
                  />
-                <Button label = 'Submit' onPress={handlePress}/>
+                <Button label = 'Submit' onPress={() => { handlePress(email,password) }}/>
                 <View style = {styles.footer}>
                     <Text style = {styles.footerText}>Not registerd???</Text>
-                    <Link href = '/auth/sign_up' asChild>
+                    <Link href = '/auth/sign_up' asChild replace >
                         <TouchableOpacity>
                             <Text style = {styles.footerLink}>Sign up here!</Text>
                         </TouchableOpacity>
